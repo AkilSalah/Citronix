b107df738b6d29622874c4e0a1bbc08989af53cb
salah <akilsalaheddine203@gmail.com>
2024-11-18 11:41:03 +0100

    add repository interface for ferme

--- src/main/java/org/aura/citronix/Repositories/FermeRepo.java
+++ src/main/java/org/aura/citronix/Repositories/FermeRepo.java
@@ -1,4 +1,9 @@
 package org.aura.citronix.Repositories;
 
-public class FermeRepo {
+import org.aura.citronix.Entities.Ferme;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FermeRepo extends JpaRepository<Ferme,Integer> {
 }
